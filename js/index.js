fcc7c2665e3703cf3d1fefee4de2d19fd2d2f2a7
Yuliia <yuliyatimosh96@ukr.net>
2019-01-31 20:26:45 +0000

    add solveQuardEq

--- js/index.js
+++ js/index.js
@@ -7,23 +7,25 @@ alert(result);
 
  function solveQuardEq(a, b, c) {
      const diskr = calcDiskr(a, b, c);
-     if (diskr = 0) {
-         return - b / 2 * a;
+     if (diskr === 0) {
+        const x = -b / (2 * a);
+         return 'x = ' + x;
          
      }
      if (diskr < 0) {
-         return null
+         return 'no solutions';
          
      } 
      if (diskr > 0) {
-         return -b + math.sqrt(diskr) / 2 * a;
-         return - b - math.sqrt(iskr) / 2 * a;
+         const x2 = (-b + Math.sqrt(diskr)) / 2 * a; 
+         const x1 = (-b - Math.sqrt(diskr)) / 2 * a;
+         return 'x1 = ' + x1 + ', x2 = ' + x2;
      }
      
      
  }
  
- return 'x1 = ' + x1 + ', x2 = ' + x2;
+ 
  
  function calcDiskr(a, b, c) {
      return b * b - 4 * a * c;
